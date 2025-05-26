c827e366dfe730b40a8fc4574fd23c558f570496
Umberto Peserico <umberto.peserico@pandev.it>
2025-05-26 14:46:49 +0200

    feat(initial): create vite project with example console.log

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    cssMinify: true,
+    minify: true,
+    lib: {
+      entry: resolve(__dirname, "src/main.js"),
+      name: "better_ui",
+      // the proper extensions will be added
+      fileName: "better_ui",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {},
+      },
+    },
+  },
+});
